8b7d30106880c1e6ca1ef8282c0229345c7cff2f
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-07-17 23:35:51 +0300

    updated JS

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 const jokeCard = document.getElementById('jokeCard');
 const joke = document.getElementById('jokeText');
 
-const getJoke = () => {
-	const xhr = new XMLHttpRequest();
-	xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
-
-	xhr.onreadystatechange = function () {
-		if (this.readyState === 4) {
-			if (this.status === 200) {
-				// console.log(JSON.parse(this.responseText).value);
-				joke.innerHTML = `<h3>${JSON.parse(this.responseText).value}</h3>`;
-			} else {
-				joke.innerHTML = `<h3> Something went Wrong... </h3>`;
-			}
-		}
-	};
-
-	xhr.send();
+const getJoke = async () => {
+	try {
+		const res = await fetch('https://api.chucknorris.io/jokes/random');
+		const data = await res.json();
+		joke.innerHTML = `<h3>${data.value}</h3>`;
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 jokeCard.addEventListener('click', () => {
